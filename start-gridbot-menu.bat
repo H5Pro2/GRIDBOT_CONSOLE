2a97cc03f582409972e3ae02b6690d44cf4314aa
H5Pro2 <H5Pro2@users.noreply.github.com>
2026-09-10 18:54:06 +0200

    Use English Gridbot Console name

--- start-gridbot-menu.bat
+++ start-gridbot-menu.bat
@@ -1,7 +1,7 @@
 @echo off
 set "ROOT=%~dp0"
 
-start "Gridbot Konsole" powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Location -LiteralPath $env:ROOT; npm run dev"
+start "Gridbot Console" powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Location -LiteralPath $env:ROOT; npm run dev"
 
 timeout /t 3 /nobreak >nul
 start "" "http://127.0.0.1:5173/"
